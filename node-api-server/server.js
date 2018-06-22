c66161d20fdde8a197a8b7110cb8a52903ca6135
Dhiren Kumar Patra <dhirenwipro@gmail.com>
2018-06-23 02:04:17 +0530

    node-api-server example

--- node-api-server/server.js
+++ node-api-server/server.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname +'/views/partials')
+app.set('view engine', hbs);
+
+hbs.registerHelper('year' ,() => {
+    return new Date().getFullYear();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var time = new Date().toString();
+    console.log(time);
+    console.log(req.method);
+    console.log(req.url);
+    let log = `The request method is ${req.method} and the URI is ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n', (error) => {
+        if(error) {
+            console.log('Error occured while appending the file ...');
+        }
+    })
+    next();
+})
+
 app.get('/', (request, response) => {
     response.send({
-        name : 'Dhiren',
-        age : 24,
-        likes : [
+        name: 'Dhiren',
+        age: 24,
+        likes: [
             'Biking',
             'Cricket',
             'Movies'
@@ -18,13 +44,19 @@ app.get('/about', (request, response) => {
     response.send('Hello Express !!');
 });
 
-app.get('/bad', (request, response) => {
-    response.send({
-        errorCode : 'ER0023',
-        errorMessage : 'Failed to get the requested resource'
+app.get('/help', (request, response) => {
+    response.render('index.hbs', {
+        user:"Dhiren",
     });
 });
 
-app.listen(3000 ,() => {
+app.get('/bad', (request, response) => {
+    response.send({
+        errorCode: 'ER0023',
+        errorMessage: 'Failed to get the requested resource'
+    });
+});
+
+app.listen(3000, () => {
     console.log('Server is running on PORT 3000 ...');
 });
